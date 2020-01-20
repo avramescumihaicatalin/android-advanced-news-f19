52b58e4c8c4e8944e805c115486a725c64333420
Avramescu Mihai-Catalin <avramescumihaicatalin@gmail.com>
2020-01-21 00:22:38 +0200

    Search with hardoded data

--- app/src/main/java/ro/atelieruldigital/news/search/SearchActivity.java
+++ app/src/main/java/ro/atelieruldigital/news/search/SearchActivity.java
@@ -1,19 +1,32 @@
 package ro.atelieruldigital.news.search;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import ro.atelieruldigital.news.App;
 import ro.atelieruldigital.news.R;
+import ro.atelieruldigital.news.model.ArticleResponse;
+import ro.atelieruldigital.news.model.NewsAPIRequests;
+import ro.atelieruldigital.news.model.WebService.NewsWebService;
+import ro.atelieruldigital.news.recycler_view.CustomHorizontalAdapter;
+import ro.atelieruldigital.news.recycler_view.CustomVerticalAdapter;
+import timber.log.Timber;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -24,6 +37,7 @@ public class SearchActivity extends AppCompatActivity {
     RecyclerView mRecyclerViewSearch;
 
     ArrayList<String> spinnerList;
+    ArrayList<ArticleResponse.Article> mArticles;
 
 
     @Override
@@ -36,7 +50,8 @@ public class SearchActivity extends AppCompatActivity {
         setDataSpinner();
         setSpinner();
 
-        initRecyclerView();
+        getDataFromServer("apple", "2020-01-12", "popularity");
+
     }
 
     private void setSpinner() {
@@ -61,8 +76,38 @@ public class SearchActivity extends AppCompatActivity {
         spinnerList.add("relevancy");
     }
 
-    private void initRecyclerView() {
+    private void getDataFromServer(String searchingString, String date, String sortBy) {
+        Retrofit newsWebServiceRetrofit = NewsWebService.getRetrofitClient();
+        NewsAPIRequests newsAPIRequests = newsWebServiceRetrofit.create(NewsAPIRequests.class);
 
+        Call<ArticleResponse> call = newsAPIRequests.queryArticles(searchingString, date, date,
+                sortBy, "534a091354c14911aa44a800e5270924");
+
+        call.enqueue(new Callback<ArticleResponse>() {
+            @Override
+            public void onResponse(@NotNull Call<ArticleResponse> call, @NotNull Response<ArticleResponse> response) {
+
+                if (response.body() != null) {
+                    ArticleResponse articleResponse = response.body();
+                    mArticles = articleResponse.getArticles();
+                    setRecyclerView();
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull Throwable t) {
+                Timber.e(t, "Failed to get data:");
+                System.out.println("Fail to GET data");
+
+            }
+        });
+    }
+
+    private void setRecyclerView() {
+        RecyclerView verticalRecyclerView = findViewById(R.id.recycler_view_search);
+        verticalRecyclerView.setLayoutManager(new LinearLayoutManager(App.getAppContext(), RecyclerView.VERTICAL, false));
+        CustomHorizontalAdapter customHorizontalAdapter = new CustomHorizontalAdapter(mArticles);
+        verticalRecyclerView.setAdapter(customHorizontalAdapter);
     }
 
 }
