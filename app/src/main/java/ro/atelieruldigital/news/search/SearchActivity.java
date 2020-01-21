9e7a436fa11a0bc0f2c8e3346bbe7982641cd360
Avramescu Mihai-Catalin <avramescumihaicatalin@gmail.com>
2020-01-21 09:13:31 +0200

    Update to Search Activity to use input data

--- app/src/main/java/ro/atelieruldigital/news/search/SearchActivity.java
+++ app/src/main/java/ro/atelieruldigital/news/search/SearchActivity.java
@@ -1,19 +1,24 @@
 package ro.atelieruldigital.news.search;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,6 +39,7 @@ public class SearchActivity extends AppCompatActivity {
     Button mButtonDatePicker;
     DatePicker mDatePicker;
     Spinner mSpinnerSortBy;
+    Button mButtonStartSearching;
     RecyclerView mRecyclerViewSearch;
 
     ArrayList<String> spinnerList;
@@ -50,7 +56,7 @@ public class SearchActivity extends AppCompatActivity {
         setDataSpinner();
         setSpinner();
 
-        getDataFromServer("apple", "2020-01-12", "popularity");
+//        getDataFromServer("apple", "2020-01-12", "popularity");
 
     }
 
@@ -66,6 +72,7 @@ public class SearchActivity extends AppCompatActivity {
         mButtonDatePicker = findViewById(R.id.button_date_picker);
         mDatePicker = findViewById(R.id.date_picker);
         mSpinnerSortBy = findViewById(R.id.spinner_sort_by);
+        mButtonStartSearching = findViewById(R.id.button_start_searching);
         mRecyclerViewSearch = findViewById(R.id.recycler_view_search);
     }
 
@@ -110,4 +117,39 @@ public class SearchActivity extends AppCompatActivity {
         verticalRecyclerView.setAdapter(customHorizontalAdapter);
     }
 
+    public void startSearchingOnClick(View view) {
+        String searchString = "";
+        String date = "";
+        String sortBy = "";
+
+        if (mEditTextInput != null) {
+            searchString = mEditTextInput.getText().toString();
+            if (TextUtils.isEmpty(searchString)) {
+                Toast.makeText(this, getResources().getString(R.string.hint_introduce_a_subject_for_searching), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        date = "2020-01-12";
+        if (mSpinnerSortBy != null) {
+            sortBy = mSpinnerSortBy.getSelectedItem().toString();
+        }
+
+        getDataFromServer(searchString, date, sortBy);
+    }
+
+    public void displayDatePickerOnClick(View view) {
+        // set today as default date
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+//        // display the date picker dialog
+        DialogFragment datePickerFragment = new DatePickerFragment();
+        ((DatePickerFragment) datePickerFragment).setYear(year);
+        ((DatePickerFragment) datePickerFragment).setMonth(month);
+        ((DatePickerFragment) datePickerFragment).setDay(day);
+        datePickerFragment.show(getSupportFragmentManager(), "DatePicker");
+        mButtonDatePicker.setText("AAAA-ZZ-LL");
+    }
 }
